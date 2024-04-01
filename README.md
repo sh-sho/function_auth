9f037d9c1a1d4da3f2e0be204055b6ef600bf1f2
sh-sho <sho.obata.c5@gmail.com>
2024-04-01 19:16:36 +0900

    add code

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# function_auth
+# OCI Vault Hands-on
+
+* 前提条件
+* ハンズオン全体像
+* 事前準備
+* OCI Vaultのハンズオン
+    * OCI Vaultの作成
+        * Master Encryption Keysの作成
+        * Secretの作成
+    * OCI Functionsの作成
+        * アプリケーションの作成
+        * コンテキストの更新
+        * Function コードの説明
+        * Functionのデプロイ
+        * Functionを用いてSecretを取得
+
+
+## 前提条件
+* Compute, OCI Functions を実行するために必要なポリシーや仮想ネットワーク(VCN)が作成済みであること
+* Cloud Shell が使用可能な環境であること
+    * 未実施の場合は、[Oracle Cloud Infrastructure ドキュメント - クラウド・シェル](#https://docs.oracle.com/ja-jp/iaas/Content/API/Concepts/cloudshellintro.htm)を参考に、必要なポリシー等を設定してください
+    * Cloud Shell/OCI Functions の CPU アーキテクチャは、X86 で統一すること
+* 各種ツール、言語、ライブラリのバージョンは、Cloud Shell に事前にインストールされているものに従います
+    * OCI CLI: 3.38.0
+    * Fn Project: 0.6.29
+    * Python: 3.8.13
+    * SDK for Python: 2.125.0
+
+## ハンズオン全体像
+
+## 事前準備
+### ハンズオンで使うコードのクローン
+ハンズオンを実施するにあたり、本リポジトリを Cloud Shell 上にクローンします。
+
+
+
+## OCI Vaultのハンズオン
+### OCI Vaultの作成
+#### Master Encryption Keysの作成
+#### Secretの作成
+### OCI Functionsの作成
+#### アプリケーションの作成
+#### コンテキストの更新
+#### Function コードの説明
+#### Functionのデプロイ
+#### FunctionのConfigurationの設定
+#### Functionを用いてSecretを取得
---
+++ sample-auth-func/func.py
@@ -0,0 +1,44 @@
+import io
+import oci
+import base64
+import json
+import logging
+
+from fdk import response
+
+signer = oci.auth.signers.get_resource_principals_signer()
+client = oci.secrets.SecretsClient({}, signer=signer)
+
+def get_text_secret(secret_ocid):
+    try:
+        get_secret = client.get_secret_bundle(secret_ocid).data.secret_bundle_content.content.encode('utf-8')
+        decrypt_secret = base64.b64decode(get_secret).decode("utf-8")
+        logging.getLogger().info(get_secret)
+    except Exception as e:
+        print("Error: failed to retrieve the secret content", e, flush=True)
+        raise
+    return {"secret content": decrypt_secret}
+
+def handler(ctx, data: io.BytesIO = None):
+    logging.getLogger().info("function start")
+
+    secret_ocid = secret_type = ""
+    try:
+        cfg = dict(ctx.Config())
+        secret_ocid = cfg["secret_ocid"]
+        logging.getLogger().info("Secret ocid = " + secret_ocid)
+        secret_type = cfg["secret_type"]
+        logging.getLogger().info("Secret type = " + secret_type)
+    except Exception as e:
+        print('ERROR: Missing configuration keys, secret ocid and secret_type', e, flush=True)
+        raise
+
+    secret_text = get_text_secret(secret_ocid)
+    logging.getLogger().debug("secret detail")
+    logging.getLogger().debug(secret_text)
+
+    logging.getLogger().debug("Inside Python Hello World function")
+    return response.Response(
+        ctx, response_data=secret_text,
+        headers={"Content-Type": "application/json"}
+    )
